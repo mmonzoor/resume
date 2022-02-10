434b73fb52787ffb2ea7f03be8c3a3dce00ae247
mmonzoor <mmonzoor.wv@gmail.com>
2022-02-09 19:31:20 -0500

    updated grad notes

--- content.py
+++ content.py
@@ -7,20 +7,36 @@ def about_content():
     4 years of experience in fraud and data with the ability to lead projects 
     from both a technical and business capacity.  As a **Sr. Data 
     Scientist** at Interac, I have prevented over **$10 million**
-    dollars in Fraud losses and saved hundreds of hours in operational time.
-    Aside from working full time, I have also been pursuing a Masters degree 
+    dollars in Fraud losses and saved hundreds of hours in operational time. Currently 
+    as a Fraud Strategist at Stripe, I develop strategies to curb losses associated 
+    with fraudulent merchants while minimizing good user pain. 
+    As for my academic background, I have just completed my Masters degree in Computer Science 
     at the University of Pennsylvania.""") 
     
-    st.write(""" *As I complete my 
-    masters degree in Computer Science, I want to utilize the knowledge 
-    I have gained in this program and take part in projects 
-    more geared towards* **software enginnering.**""")
+    st.write(""" *After completeing my degree at Penn Engineering, I have embarked on the journey to 
+    transition into a  **software enginnering** role.*""")
 
-    st.markdown("""If you think you can help me in my journey, or have a 
-    project I can help you with, please get in [touch](https://www.linkedin.com/in/mmonzoor/)! """)
+    st.markdown("""If you like my background, then let's [connect](https://www.linkedin.com/in/mmonzoor/)! """)
+
 def experience():
     st.title("Experience")
-    st.subheader("Sr. Data Scientist | [Interac Corp.](https://newsroom.interac.ca/video-interac-up-close-mumtahin-monzoor/)| 2017/01 - Present")
+    
+    st.subheader("Strategist | Stripe | 2021/06 - Present")
+    st.markdown("""
+    - **Identity Fraud**: Led the strategic initiative to stop networks of bad actors spinning up 
+    fraudulent accounts at Stripe and contributing to millions in losses.
+    - **Distributed Fraud pattern**: Launched improved fraud interventions in markets with increased level of contribution towards
+    distribted fraud attacks which has decreased good user pain by **30%** and increased loss savings by **$2 million**.""")
+
+    #https://practical-davinci-2538f5.netlify.app/
+    st.subheader("Founder | Unipod | 2020/03 - 2021/08")
+    st.markdown("""
+    - Ideated on educational learning tools to build for students joining remote learning at the height of the pandemic.
+    - Conducted 100 hours of customer interviews to understand remote learning needs identified by students and parents. 
+    - Built a [grade predictor application](https://practical-davinci-2538f5.netlify.app/) to be used by local high school students on calculating their assignment grades to reach their GPA goals. 
+    Even though we were able to reach monthly users, we struggled to find a proper product market fit for the long term and wrapped up the initiative at the end of 2021.""")
+
+    st.subheader("Sr. Data Scientist | [Interac Corp.](https://newsroom.interac.ca/video-interac-up-close-mumtahin-monzoor/)| 2017/01 - 2021/05")
     st.markdown("""
     - **Scam Detection**: Created a detection model targeting scammers exploiting Covid-19 
     relief funds and blocking $1,000,000 in scam attempts over 24 hours.
@@ -51,6 +67,8 @@ def experience():
 def projects():
     st.title("Projects")
     st.markdown("""
+    - **What do I need to get an A?**: I wrote a grade [calculator](https://practical-davinci-2538f5.netlify.app/) that tells a student exactly what 
+    score is needed for each assessment to reach a target grade.  
     - **Got an Edge?**: I wrote a Streamlit app (similar to this resume) that introduces 
     the concept of edge detection in Computer Vision. Feel free
     to check out the [app](https://share.streamlit.io/mmonzoor/edge_detection/app.py) 
@@ -60,10 +78,6 @@ def projects():
     for exploratory analysis. If you are interested, check out my [repo](
     https://github.com/mmonzoor/introductory_pot_hole_viz) and my [article](
     https://towardsdatascience.com/mapping-locations-of-reported-pot-holes-in-toronto-using-python-376402d8da53?sk=092fc3bf03d3d0cdae6d0751e82b04af).
-    - **Fetch Data from API**: I created teaching content and workshop tutorials 
-    for University of Waterloo's Big Data in Social Science course offered by 
-    Dr. John McLevey. Curated content explaining API usage which can be found 
-    in this [repo](https://github.com/mmonzoor/computational_social_science_course_notebooks/tree/master/APIs).
     - **Blockbuster Nostalgia**: I created a Virtual Movie Rental store as a throw back to 
     the ol' days of Blockbuster. The project was written in Java and I used the
     IMDB API to fetch movie records and mimicked the flow of an e-commerce store 
